3ee4e030873781a642032ab30869b862cd91938c
OrlandoR54 <46764632+OrlandoR54@users.noreply.github.com>
2019-12-01 01:58:47 -0500

    Update modificarContraseniaUser.php

--- Public/Controlador/modificarContraseniaUser.php
+++ Public/Controlador/modificarContraseniaUser.php
@@ -9,21 +9,25 @@
         <head>
             <meta charset="UTF-8">
             <title>Cambiar Contrasena</title>
+            <link rel="stylesheet" href="../../css/modificaUser.css">
         </head>
         <body>
             <?php
                 $rol_admin = $_GET["rol_admin"];
                 $codigo = $_GET["codigo"];
             ?>
+            
             <form class="box" method="POST" action="../../Private/Controlador/modificar_contrasena_user.php?rol_admin=<?php echo $rol_admin ?>">
                 <input type="hidden" id="codigo" name="codigo" value="<?php echo $codigo ?>">
-    
-                <label class="contrasena" for="contrasenaNueva">Contrasena Nueva (*)</label>
-                <input type="password" id="contrasena2" name="contrasena2" value="" required placeholder="Ingrese su contrasena nueva...">
+                <div class="inf">
+                    <label class="contrasena" for="contrasenaNueva">Contrasena Nueva (*)</label>
+                    <input class="txtUser" type="password" id="contrasena2" name="contrasena2" value="" required placeholder="Ingrese su contrasena nueva...">
+                </div>
                 <br>
-    
-                <input class="boton" type="submit" id="modificar" name="modificar" value="Modificar">
-                <input type="button" id="cancelar" name="cancelar" value="Cancelar" onclick="location.href='../../Private/Controlador/gestion_user.php?rol_admin=<?php echo $rol_admin ?>'" class="boton">
+                <div class= "botones">
+                    <input class="boton" type="submit" id="modificar" name="modificar" value="Modificar">
+                    <input class="boton" type="button" id="cancelar" name="cancelar" value="Cancelar" onclick="location.href='../../Private/Controlador/gestion_user.php?rol_admin=<?php echo $rol_admin ?>'" class="boton">
+                </div>
             </form>
         </body>
     </html>
