2322c3b9cb99a691a19a71a79b6a37ad6120a3b7
OrlandoR54 <46764632+OrlandoR54@users.noreply.github.com>
2019-12-02 18:04:43 -0500

    Update index.php

--- Private/Controlador/index.php
+++ Private/Controlador/index.php
@@ -17,6 +17,7 @@ $rol_admin = $_GET['rol_admin'];
     <title>Pagina principal administrador</title> 
     <link rel="stylesheet" href="../../css/styles.css">
     <link rel="stylesheet" href="../../css/structure.css">
+    <link rel="stylesheet" href="../../css/catalogo.css">
 </head>
 
 <body>
@@ -24,31 +25,75 @@ $rol_admin = $_GET['rol_admin'];
     <!-- Barra navegador (acentada arriba) -->
     <div class="cabecera">
         <div class="barra cabColor espAmplio margRelleno sombra">
-            <a href="../../Public/Vista/index.html" class="barraItem boton"><b>CBD</b> Cannabidiol</a>
+            <a href="index.php?rol_admin=<?php echo $rol_admin ?>" class="barraItem boton"><b>CBD</b> Cannabidiol</a>
             <!-- enlaces flotantes a la derecha. Econdiendoles en una pantallas pequeñas -->
             <div class="derecha">
                 <a href="index.php?rol_admin=<?php echo $rol_admin ?>" class="barraItem boton">Inicio</a>
-                <a href="gestion_user.php?rol_admin=<?php echo $rol_admin ?>" class="barraItem boton">Gestionar Usuarios</a>
-                <a href="GestionProductos/gestion_productos.php?rol_admin=<?php echo $rol_admin ?>" class="barraItem boton">Gestionar Productos</a>
-                <a href="envios.php?rol_admin=<?php echo $rol_admin ?>"class="barraItem boton" >Gestion Pedidos</a>
-
-                <a href="GestionProductos/gestion_comentarios.php?rol_admin=<?php echo $rol_admin ?>" class="barraItem boton">Gestionar Comentarios</a>
-                <a href="GestionFacturas/gestion_factura.php?rol_admin=<?php echo $rol_admin ?>" class="barraItem boton">Gestionar Facturas</a>
-
-                
                 <a href="../../config/Cerrar_Sesion.php" class="barraItem boton">&#128682;Cerrar Sesion</a>
             </div>
         </div>
     </div>
 
-    <center>
-        <div>
-            <h1>Bienvenido</h1>
-            <h2>Usuario Administrador</h2>
-        </div>
-    </center>
 
-   
+
+
+    <div class="seccion">
+        <div class="barra-lateral barra-block barra-card w3-animate-left" style="display:none" id="mySidebar">
+            <button class="w3-bar-item w3-button w3-large" onclick="w3_close()">Close &times;</button>
+            <br>
+            <a href="gestion_user.php?rol_admin=<?php echo $rol_admin ?>" class="w3-bar-item w3-button">Gestionar Usuarios</a>
+            <a href="GestionProductos/gestion_productos.php?rol_admin=<?php echo $rol_admin ?>" class="w3-bar-item w3-button">Gestionar Productos</a>
+            <a href="GestionProductos/gestion_comentarios.php?rol_admin=<?php echo $rol_admin ?>" class="w3-bar-item w3-button">Gestionar Comentarios</a>
+            <a href="envios.php?rol_admin=<?php echo $rol_admin ?>" class="w3-bar-item w3-button">Gestion Pedidos</a>
+            <a href="GestionFacturas/gestion_factura.php?rol_admin=<?php echo $rol_admin ?>" class="w3-bar-item w3-button">Gestionar Facturas</a>
+        </div>
+
+        <div id="main">
+        
+            <div class="w3-teal">
+                <button id="openNav" class="w3-button w3-teal w3-xlarge" onclick="w3_open()">&#9776;</button>
+                <div class="w3-container">
+                    <h1>Bienvenido</h1>
+                    <h2>Usuario Administrador</h2>
+                </div>
+            </div>
+
+            <!--Comienzo-->
+            <div class="w3-container" id="aceite">
+                <!--  <div class="w3-container w3-text-grey" id="jeans">
+                    <p> </p>
+                </div> -->
+
+                   
+
+
+            </div>
+            <!----Final--->
+        </div>
+    </div>
+
+
+    <script>
+        function w3_open() {
+            document.getElementById("main").style.marginLeft = "20%";
+            document.getElementById("mySidebar").style.width = "20%";
+            document.getElementById("mySidebar").style.display = "block";
+            document.getElementById("openNav").style.display = 'none';
+        }
+        function w3_close() {
+            document.getElementById("main").style.marginLeft = "0%";
+            document.getElementById("mySidebar").style.display = "none";
+            document.getElementById("openNav").style.display = "inline-block";
+        }
+    </script>
+
+
+
+
+
+
+
+
 </body>
 
 </html>
