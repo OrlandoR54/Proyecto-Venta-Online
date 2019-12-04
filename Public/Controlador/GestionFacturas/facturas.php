1da8a3c6193861a7ea6e422cead1ce32fd61cfab
OrlandoR54 <46764632+OrlandoR54@users.noreply.github.com>
2019-12-04 12:45:44 -0500

    Update facturas.php

--- Public/Controlador/GestionFacturas/facturas.php
+++ Public/Controlador/GestionFacturas/facturas.php
@@ -20,7 +20,9 @@ if (!isset($_SESSION['isLogged']) || $_SESSION['isLogged'] === FALSE) {
 <head>
     <meta charset="UTF-8">
     <title>Mis Facturas</title>
-    
+    <link rel="stylesheet" href="../../../css/styles.css">
+    <link rel="stylesheet" href="../../../css/structure.css">
+    <link rel="stylesheet" href="../../../css/catalogo.css">
 </head>
 
 <body>
@@ -31,54 +33,65 @@ if (!isset($_SESSION['isLogged']) || $_SESSION['isLogged'] === FALSE) {
 
    
     ?>
+
+    <!-- Barra navegador (acentada arriba) -->
+    <div class="cabecera">
+        <div class="barra cabColor espAmplio margRelleno sombra">
+            <a href="../indexUser.php?codigo=<?php echo $codigo ?>" class="barraItem boton"><b>CBD</b> Cannabidiol</a>
+            <!-- enlaces flotantes a la derecha. Econdiendoles en una pantallas pequeñas -->
+            <div class="derecha">
+                <a href="../indexUser.php?codigo=<?php echo $codigo ?>" class="barraItem boton">Home</a>
+                <a href="../../../Private/Controlador/GestionUsuario/mi_cuenta.php?codigo=<?php echo $codigo ?>" class="barraItem boton">Mi Cuenta</a>
+                <a href="../../../config/cerrar_sesion.php" class="barraItem boton">&#128682;Cerrar Sesion</a>
+            </div>
+        </div>
+    </div>
+
+    <!--------------------------------------------------------------------------------------------------->
+
+
     <header class="header">
-        <nav>
-            <ul> <li><a href="index.php?codigo=<?php echo $per_id ?>">Inicio</a></li>
-                <li><a href="../../../config/cerrar_sesion.php">Cerrar Sesion</a></li>
-            </ul>
-        </nav>
+        <h1>Mis Facturas</h1>
     </header>
     <main class="main">
         <table id="buzon" class="tg" style="undefined;table-layout: fixed; width: 1062px">
             
-        <colgroup>
-                    <col style="width: 100px">
-                    <col style="width: 100px">
-                    <col style="width: 100px">
-                    <col style="width: 100px">
-                 </colgroup>
-                    <tr>
-                    <th class="tg-lboi">Numero de factura:</th>
-                    <th class="tg-lboi">Fecha de venta:</th>
-                    <th class="tg-lboi">Total de la factura:</th>
-                    <th class="tg-lboi">Accion:</th>
-                    </tr>
+            <colgroup>
+                <col style="width: 100px">
+                <col style="width: 100px">
+                <col style="width: 100px">
+                <col style="width: 100px">
+            </colgroup>
+            <tr>
+                <th class="tg-lboi">Numero de factura:</th>
+                <th class="tg-lboi">Fecha de venta:</th>
+                <th class="tg-lboi">Total de la factura:</th>
+                <th class="tg-lboi">Accion:</th>
+            </tr>
 
                     
-                    <?php
+            <?php
      
-            include '../../../config/conexionBD.php';
+ 
           
-            $sql = "SELECT * FROM mh_fact_cabec_vent WHERE mh_persons_per_id ='$codigo' and fc_vent_estado='F'";
-            $result = $conn->query($sql);
+                $sql = "SELECT * FROM mh_fact_cabec_vent WHERE mh_persons_per_id ='$codigo' and fc_vent_estado='F'";
+                $result = $conn->query($sql);
 
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                  
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                    
                         echo "<tr>";
                         echo "<td>" . $row["fc_vent_id"] . "</td>";
                         echo "<td>" . $row["fc_fecha_vent"] . "</td>";
                         echo "<td>" . $row["fc_vent_total"] . "</td>";
                         echo "<td class='accion'><a href='verFactura.php?codigo=" . $row['mh_persons_per_id'] . "'>Modificar</a></td>";
-                       
-                    
+                    }
+                } else {
+                    echo "<tr>";
+                    echo "<td colspan='4'>No existen facturas</td>";
+                    echo "</tr>";
                 }
-            } else {
-                echo "<tr>";
-                echo "<td colspan='4'>No existen facturas</td>";
-                echo "</tr>";
-            }
-            $conn->close();
+                $conn->close();
             ?>
         </table>
 </body>
