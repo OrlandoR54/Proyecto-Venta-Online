40607fce0ca6d94dd79db307e3894cac72986881
OrlandoR54 <46764632+OrlandoR54@users.noreply.github.com>
2019-12-04 11:16:32 -0500

    Update modificarContrasenia.php

--- Public/Controlador/GestionUsuario/modificarContrasenia.php
+++ Public/Controlador/GestionUsuario/modificarContrasenia.php
@@ -9,7 +9,7 @@
         <head>
             <meta charset="UTF-8">
             <title>Cambiar Contrasena</title>
-            <link rel="stylesheet" href="../../css/modificaUser.css">
+            <link rel="stylesheet" href="../../../css/modificaUser.css">
         </head>
         <body>
             <?php
@@ -19,13 +19,13 @@
             <form class="box" method="POST" action="../../../Private/Controlador/GestionUsuario/modificar_contrasenia.php?codigo=<?php echo $codigo ?>">
                 <input type="hidden" id="codigo" name="codigo" value="<?php echo $codigo ?>">
                 <div class="inf">
-                    <label class="contrasena" for="contrasenaNueva">Contrasena Nueva (*)</label>
+                    <label class="modificar" for="contrasenaNueva">Contrasena Nueva (*)</label>
                     <input class="txtUser" type="password" id="contrasena2" name="contrasena2" value="" required placeholder="Ingrese su contrasena nueva...">
                 </div>
                 <br>
                 <div class= "botones">
                     <input class="boton" type="submit" id="modificar" name="modificar" value="Modificar">
-                    <input class="boton" type="button" id="cancelar" name="cancelar" value="Cancelar" onclick="location.href='.../../Private/Controlador/gestion_user.php?rol_admin=<?php echo $rol_admin ?>'" class="boton">
+                    <input class="boton" type="button" id="cancelar" name="cancelar" value="Cancelar" onclick="location.href='../../../Private/Controlador/GestionUsuario/mi_cuenta.php?codigo=<?php echo $codigo ?>'" class="boton">
                 </div>
             </form>
         </body>
