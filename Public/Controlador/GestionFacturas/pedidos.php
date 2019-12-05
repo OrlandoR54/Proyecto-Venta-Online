58bb1555ac28fbb30de6fa01624c0866b6f3c071
Hugo Zhindon <35283790+HugoDavid64@users.noreply.github.com>
2019-12-05 02:02:37 -0500

    Gestion Factura y Gestion Pedido

---
+++ Public/Controlador/GestionFacturas/pedidos.php
@@ -0,0 +1,99 @@
+<?php
+session_start();
+if (!isset($_SESSION['isLogged']) || $_SESSION['isLogged'] === FALSE) {
+    header("Location: /SistemaDeGestion/public/vista/login.html");
+}
+?>
+<!DOCTYPE html>
+<html>
+
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;border-color:#aabcfe;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#aabcfe;color:#669;background-color:#e8edff;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#aabcfe;color:#039;background-color:#b9c9fe;}
+.tg .tg-lboi{border-color:inherit;text-align:left;vertical-align:middle}
+.tg .tg-6p27{color:#fe0000;border-color:inherit;text-align:left;vertical-align:middle}
+.tg .tg-0pky{border-color:inherit;text-align:left;vertical-align:top}
+</style>
+
+
+<head>
+    <meta charset="UTF-8">
+    <title>Mis Facturas</title>
+    <link rel="stylesheet" href="../../../css/styles.css">
+    <link rel="stylesheet" href="../../../css/structure.css">
+    <link rel="stylesheet" href="../../../css/catalogo.css">
+</head>
+
+<body>
+    <?php
+    include '../../../config/conexionBD.php';
+   
+    $codigo = $_GET["codigo"];
+
+   
+    ?>
+
+    <!-- Barra navegador (acentada arriba) -->
+    <div class="cabecera">
+        <div class="barra cabColor espAmplio margRelleno sombra">
+            <a href="../indexUser.php?codigo=<?php echo $codigo ?>" class="barraItem boton"><b>CBD</b> Cannabidiol</a>
+            <!-- enlaces flotantes a la derecha. Econdiendoles en una pantallas pequeñas -->
+            <div class="derecha">
+                <a href="../indexUser.php?codigo=<?php echo $codigo ?>" class="barraItem boton">Home</a>
+                <a href="../../../Private/Controlador/GestionUsuario/mi_cuenta.php?codigo=<?php echo $codigo ?>" class="barraItem boton">Mi Cuenta</a>
+                <a href="../../../config/cerrar_sesion.php" class="barraItem boton">&#128682;Cerrar Sesion</a>
+            </div>
+        </div>
+    </div>
+
+    <!--------------------------------------------------------------------------------------------------->
+
+
+    <header class="header">
+        <h1>Mis Facturas</h1>
+    </header>
+    <main class="main">
+        <table id="buzon" class="tg" style="undefined;table-layout: fixed; width: 1062px">
+            
+            <colgroup>
+                <col style="width: 100px">
+                <col style="width: 100px">
+                <col style="width: 100px">
+                <col style="width: 100px">
+            </colgroup>
+            <tr>
+                <th class="tg-lboi">Numero de factura:</th>
+                <th class="tg-lboi">Fecha de venta:</th>
+                <th class="tg-lboi">Total de la factura:</th>
+                <th class="tg-lboi">Accion:</th>
+            </tr>
+
+                    
+            <?php
+     
+ 
+          
+                $sql = "SELECT * FROM mh_fact_cabec_vent WHERE mh_persons_per_id ='$codigo' and fc_vent_estado='A'";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                    
+                        echo "<tr>";
+                        echo "<td>" . $row["fc_vent_id"] . "</td>";
+                        echo "<td>" . $row["fc_fecha_vent"] . "</td>";
+                        echo "<td>" . $row["fc_vent_total"] . "</td>";
+                        echo "<td class='accion'><a href='verFactura.php?codigo=" . $row['mh_persons_per_id'] . "'>Modificar</a></td>";
+                    }
+                } else {
+                    echo "<tr>";
+                    echo "<td colspan='4'>No existen facturas</td>";
+                    echo "</tr>";
+                }
+                $conn->close();
+            ?>
+        </table>
+</body>
+
+</html>
--- Public/Productos/Pagar.php
+++ Public/Productos/Pagar.php
@@ -23,11 +23,11 @@ $codigo = $_GET["codigo"];
                          $result = $conn->query($sql1);
                          if($result->num_rows >0){
                              while($row=$result->fetch_assoc()){
-                                echo "Suma",$total=$row["SUM(carri_subt)"] ;
+                               $total=$row["SUM(carri_subt)"] ;
                                 $fechaActual = date('Y-m-d');
                              }
                          }else {  
-                            echo "No hay valores a pagar";  
+                            /*echo "No hay valores a pagar";  */
                          }           
             /* Recuperar productos*/
             $sql2 = "SELECT prod_nombre,SUM(carri_subt), count(mh_products_prod_id),	carrit_fecha_compr 
@@ -53,20 +53,22 @@ $codigo = $_GET["codigo"];
             VALUES (NULL,'$fechaActual',0.12,'$total','$codigo','A','N') ";
             $result = $conn->query($sql3);   
             if ($conn->query($sql2) == TRUE){
-                echo "Cabecera Ingresada";
+              /*  echo "Cabecera Ingresada";*/
              } else{
-                    echo "No se ingreso la cabecera";
+                  /*  echo "No se ingreso la cabecera";*/
              } 
 
              /**Recuperar id de la factura cabecera */
-           $sql4="SELECT `fd_vent_id` FROM `mh_detal_vent`, `mh_fact_cabec_vent` WHERE $codigo=mh_persons_per_id";
+           $sql4="SELECT `fc_vent_id` FROM `mh_detal_vent`, `mh_fact_cabec_vent` 
+           WHERE mh_persons_per_id=$codigo 
+           ORDER BY mh_persons_per_id DESC";
              $result = $conn->query($sql4);
              if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
-                    $idcabecera=$row["fd_vent_id"];
+                   echo "ID DE LA FACTURA CABECERA", $idcabecera=$row["fc_vent_id"];
                 }
             }  else{
-                echo "No recupero id de la cabecera";
+                /*echo "No recupero id de la cabecera";*/
             }   
 
                /* Recuperar productos*/
@@ -79,18 +81,18 @@ $codigo = $_GET["codigo"];
                $result = $conn->query($sql5);
                if ($result->num_rows > 0) {
                    while ($row = $result->fetch_assoc()) {
-                       echo $IDdelProductoA=$row["mh_products_prod_id"] ;
-                        echo $SubtotalCadaProductoA=$row["carri_subt"] ;
-                        echo $cant=$row["carrit_prod_vent"];
+                       $IDdelProductoA=$row["mh_products_prod_id"] ;
+                       $SubtotalCadaProductoA=$row["carri_subt"] ;
+                         $cant=$row["carrit_prod_vent"];
                         /*Ingresar Detalles Factura */
                        $sql6="INSERT INTO `mh_detal_vent`(`fd_vent_id`, `fd_vent_cantidad`, `fd_vent_precio`,
                         `mh_fact_cabec_vent_fc_vent_id`, `fc_vent_subtotal`, `mh_products_prod_id`, `fd_vent_total`)
                         VALUES (NULL,'$cant','$SubtotalCadaProductoA','$idcabecera', '$SubtotalCadaProductoA', '$IDdelProductoA', '$SubtotalCadaProductoA')";
                         $result1 = $conn->query($sql6);
                         if ($conn->query($sql6) == TRUE){
-                        echo "Detalle ingresado";
+                       /* echo "Detalle ingresado";*/
                         } else{
-                            echo "No se ingreso el detalle";
+                           /* echo "No se ingreso el detalle";*/
                         }  
                        
                    }
@@ -102,9 +104,9 @@ $codigo = $_GET["codigo"];
                WHERE $idcabecera=fc_vent_id";
                 $result2 = $conn->query($sql7);
                 if ($conn->query($sql7) == TRUE){
-                    echo "Total Modificado";
+                    /*echo "Total Modificado";
                  } else{
-                        echo "No se modifica el total";
+                        echo "No se modifica el total";*/
                  } 
 
                    /**Recuperar id de la factura cabecera */
@@ -112,7 +114,7 @@ $codigo = $_GET["codigo"];
                 $result3 = $conn->query($sql8);
                 if ($result3->num_rows > 0) {
                 while ($row = $result3->fetch_assoc()) {
-                 echo "ID CARRITO DE COMPRA" ,$idcarrito=$row["carrit_id"];
+                 $idcarrito=$row["carrit_id"];
                 }
                 }else{
                      echo "No recupero id de la cabecera";
@@ -122,6 +124,10 @@ $codigo = $_GET["codigo"];
                  $result4 = $conn->query($sql9);
                 if ($conn->query($sql9) == TRUE){
                     echo "Eliminado";
+                    echo "<script type= 'text/javascript'>
+                    alert('El pago se efectuo con exito');
+                     location='../Controlador/catalogoUser.php?codigo=$codigo';
+                     </script>";
                  } else{
                         echo "No eliminado";
                  } 
