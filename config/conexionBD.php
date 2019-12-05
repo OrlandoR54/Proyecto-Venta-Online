336d17792bffbcd8a4ee1602a2186b4a1161ae83
OrlandoR54 <46764632+OrlandoR54@users.noreply.github.com>
2019-12-05 15:22:38 -0500

    Update conexionBD.php

--- config/conexionBD.php
+++ config/conexionBD.php
@@ -1,8 +1,8 @@
 <?php
 $db_servername = "localhost";
-$db_username = "root";
-$db_userpassword = "";
-$db_name = "hipermedialproyecto";
+$db_username = "u755686168_root";
+$db_userpassword = "@dm1n.2019";
+$db_name = "u755686168_ventas";
 
 $conn = new mysqli($db_servername,$db_username,$db_userpassword,$db_name);
 $conn->set_charset("utf8");
@@ -12,5 +12,5 @@ if($conn->connect_error){
     die("connnection falied ".$conn->connect_error);
 
 }else{
-   echo "<p> Conexion exitosa!</p>";
+   //echo "<p> Conexion exitosa!</p>";
 }
