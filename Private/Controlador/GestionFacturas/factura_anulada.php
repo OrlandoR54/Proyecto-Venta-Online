2bd3e3cd00f42e216dca76dab3c3009be8240a93
Kevin Cordero <46872883+wonejo92@users.noreply.github.com>
2019-12-04 22:11:52 -0500

    Metodo para anular la factura

---
+++ Private/Controlador/GestionFacturas/factura_anulada.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Eliminar datos del usuario</title>
+    
+</head>
+
+<body>
+    <?php
+    //Incluir conexion a la BD
+    include '../../../config/conexionBD.php';
+    $codigo =$_GET['codigo'];
+    $sql = "UPDATE mh_fact_cabec_vent SET fc_estado = 'S' WHERE fc_vent_id = '$codigo'";
+    if ($conn->query($sql) == TRUE) {
+        echo "<p>Se ha eliminado los datos correctamente</p>";
+    } else {
+        echo "<p>Error" . $sql . "<br>" . mysqli_error($conn) . "</p>";
+    }
+    echo "<Regresar</a>";
+    $conn->close();
+    ?>
+</body>
+
+</html>
