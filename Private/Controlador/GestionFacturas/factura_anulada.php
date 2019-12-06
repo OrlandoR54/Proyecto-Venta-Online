3d6262044eb7a65a3e037d7a840e2e01bd898aeb
OrlandoR54 <46764632+OrlandoR54@users.noreply.github.com>
2019-12-05 21:37:31 -0500

    Update factura_anulada.php

--- Private/Controlador/GestionFacturas/factura_anulada.php
+++ Private/Controlador/GestionFacturas/factura_anulada.php
@@ -13,13 +13,14 @@
     //comentario para hacer el push Funcioanado
     include '../../../config/conexionBD.php';
     $codigo =$_GET['codigo'];
+    $rol_admin = $_GET['rol_admin'];
     $sql = "UPDATE mh_fact_cabec_vent SET fc_estado = 'S' WHERE fc_vent_id = '$codigo'";
     if ($conn->query($sql) == TRUE) {
         echo "<p>Se ha eliminado los datos correctamente</p>";
     } else {
         echo "<p>Error" . $sql . "<br>" . mysqli_error($conn) . "</p>";
     }
-    echo "<Regresar</a>";
+    header("Location:gestion_factura.php?codigo=<?php echo $codigo ?>?rol_admin=<?php echo $rol_admin ?>");
     $conn->close();
     ?>
 </body>
