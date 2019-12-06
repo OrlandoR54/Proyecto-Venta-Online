8881c75d25179518e05a314eb535911268ade8b5
OrlandoR54 <46764632+OrlandoR54@users.noreply.github.com>
2019-12-06 01:41:29 -0500

    Update Cerrar_Sesion.php

--- config/Cerrar_Sesion.php
+++ config/Cerrar_Sesion.php
@@ -2,5 +2,5 @@
     session_start();
     $_SESSION['isLogged'] = FALSE;
     session_destroy();
-    header("Location: Proyecto-Venta-Online/Public/Vista/login.html");
+    header("Location: /Public/Vista/login.html");
 ?>
