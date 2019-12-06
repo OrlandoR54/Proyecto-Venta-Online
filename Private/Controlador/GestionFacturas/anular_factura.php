8e33427007c0d122c34e52e708e4f0d24032e129
OrlandoR54 <46764632+OrlandoR54@users.noreply.github.com>
2019-12-05 21:37:28 -0500

    Update anular_factura.php

--- Private/Controlador/GestionFacturas/anular_factura.php
+++ Private/Controlador/GestionFacturas/anular_factura.php
@@ -28,8 +28,8 @@ if (!isset($_SESSION['isLogged']) || $_SESSION['isLogged'] === FALSE) {
 <body>
 <?php
     include '../../../config/conexionBD.php';
-$rol_admin = $_GET['rol_admin'];
-$codigo =$_GET['codigo'];
+    $rol_admin = $_GET['rol_admin'];
+    $codigo =$_GET['codigo'];
 ?>
     <!-- Barra navegador (acentada arriba) -->
     <div class="cabecera">
@@ -39,65 +39,86 @@ $codigo =$_GET['codigo'];
             <div class="derecha">
                 <a href="../index.php?rol_admin=<?php echo $rol_admin ?>" class="barraItem boton">Inicio</a>
                 <a href="../gestion_user.php?rol_admin=<?php echo $rol_admin ?>" class="barraItem boton">Gestionar Usuarios</a>
-                <a href="GestionProductos/gestion_productos.php?rol_admin=<?php echo $rol_admin ?>" class="barraItem boton">Gestionar Productos</a>
+                <a href="../envios.php?rol_admin=<?php echo $rol_admin ?>" class="barraItem boton">Gestion Pedidos</a>
                 <a href="../../../config/Cerrar_Sesion.php" class="barraItem boton">&#128682;Cerrar Sesion</a>
             </div>
         </div>
     </div>
 
 
-    <main class="main">
-        </section>
-        <table id="buzon" class="tg" style="undefined;table-layout: fixed; width: 1062px">
-        <div>
-        <colgroup>
-                    <col style="width: 105px">
-                    <col style="width: 120px">
-                    <col style="width: 130px">
-                    <col style="width: 150px">
-                    <col style="width: 121px">
-                    <col style="width: 200px">
-                    <col style="width: 106px">
-                    <col style="width: 95px">
-                    <col style="width: 95px">
-                    <col style="width: 95px">
-                 </colgroup>
-                    <tr>
-                    <th class="tg-lboi">Nombre</th>
-                    <th class="tg-lboi">Apellido</th>
-                    <th class="tg-lboi">Fecha Venta</th>
-                    <th class="tg-lboi">Iva</th>
-                    <th class="tg-lboi">Total</th>
-                    </tr>
-        </div>
-            <?php
 
-            $sql = "SELECT * FROM mh_persons,mh_fact_cabec_vent WHERE mh_persons.per_id=mh_fact_cabec_vent.mh_persons_per_id and fc_vent_id='$codigo'";
-            //cometario para el push Funcionanado
-            $result = $conn->query($sql);
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                        echo "<tr>";
-                        echo "<td>" . $row["per_nombre"] . "</td>";
-                        echo "<td>" . $row["per_apellido"] . "</td>";
-                        echo "<td>" . $row["fc_fecha_vent"] . "</td>";
-                        echo "<td>" . $row["fc_vent_iva"] . "</td>";
-                        echo "<td>" . $row["fc_vent_total"] . "</td>";
-                }
-            } else {
-                echo "<tr>";
-                echo "<td colspan='10'>No existen usuarios</td>";
-                echo "</tr>";
-            }
-            $conn->close();
-            ?>
-        </table>
-        <form  method="POST" action="factura_anulada.php?codigo=<?php echo $codigo ?>?rol_admin=<?php echo $rol_admin ?>" >
-        <div class= "botones">
-                    <input class="boton" type="submit" id="eliminar" name="eliminar" value="Anular"  >
-                    <input class="boton" type="button" id="cancelar" name="cancelar" value="Cancelar" onclick="location.href='gestion_factura.php?rol_admin=<?php echo $rol_admin ?>'" class="boton">
+    <div class="seccion">
+
+        <div id="main">
+        
+            <div class="w3-teal">
+                <div class="w3-container">
+                    <h1>Anular Factura</h1>
                 </div>
-                </form>
+            </div>
+
+            <!--Comienzo-->
+            <div class="w3-container" id="aceite">
+                <!--  <div class="w3-container w3-text-grey" id="jeans">
+                    <p> </p>
+                </div> -->
+
+                <table id="buzon" class="tg" style="undefined;table-layout: fixed; width: 1062px">
+                    <div>
+                        <colgroup>
+                            <col style="width: 105px">
+                            <col style="width: 120px">
+                            <col style="width: 130px">
+                            <col style="width: 150px">
+                            <col style="width: 121px">
+                            <col style="width: 200px">
+                            <col style="width: 106px">
+                            <col style="width: 95px">
+                            <col style="width: 95px">
+                            <col style="width: 95px">
+                        </colgroup>
+                        <tr>
+                            <th class="tg-lboi">Nombre</th>
+                            <th class="tg-lboi">Apellido</th>
+                            <th class="tg-lboi">Fecha Venta</th>
+                            <th class="tg-lboi">Iva</th>
+                            <th class="tg-lboi">Total</th>
+                        </tr>
+                    </div>
+                    <?php
+
+                        $sql = "SELECT * FROM mh_persons,mh_fact_cabec_vent WHERE mh_persons.per_id=mh_fact_cabec_vent.mh_persons_per_id and fc_vent_id='$codigo'";
+                        //cometario para el push Funcionanado
+                        $result = $conn->query($sql);
+                        if ($result->num_rows > 0) {
+                            while ($row = $result->fetch_assoc()) {
+                                    echo "<tr>";
+                                    echo "<td>" . $row["per_nombre"] . "</td>";
+                                    echo "<td>" . $row["per_apellido"] . "</td>";
+                                    echo "<td>" . $row["fc_fecha_vent"] . "</td>";
+                                    echo "<td>" . $row["fc_vent_iva"] . "</td>";
+                                    echo "<td>" . $row["fc_vent_total"] . "</td>";
+                            }
+                        } else {
+                            echo "<tr>";
+                            echo "<td colspan='10'>No existen usuarios</td>";
+                            echo "</tr>";
+                        }
+                        $conn->close();
+                    ?>
+                </table>
+                <form  method="POST" action="factura_anulada.php?codigo=<?php echo $codigo ?>?rol_admin=<?php echo $rol_admin ?>" >
+                    <div class= "botones">
+                        <input class="boton" type="submit" id="eliminar" name="eliminar" value="Anular"  >
+                        <input class="boton" type="button" id="cancelar" name="cancelar" value="Cancelar" onclick="location.href='gestion_factura.php?rol_admin=<?php echo $rol_admin ?>'" class="boton">
+                     </div>
+                </form>   
+
+            </div>
+            <!----Final--->
+        </div>
+    </div>
+
 </body>
 
 </html>
