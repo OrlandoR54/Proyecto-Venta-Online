a4d897c4ce1aec854ec09772014d74a4feda7227
OrlandoR54 <46764632+OrlandoR54@users.noreply.github.com>
2019-12-06 00:34:33 -0500

    Update verFactura.php

--- Public/Controlador/GestionFacturas/verFactura.php
+++ Public/Controlador/GestionFacturas/verFactura.php
@@ -43,152 +43,179 @@ if (!isset($_SESSION['isLogged']) || $_SESSION['isLogged'] === FALSE) {
             </div>
         </div>
     </div>
+
+
+    <div class="seccion">
+
+        <div id="main">
+        
+            <div class="w3-teal">
+                <div class="w3-container">
+                    <h1>Factura</h1>
+                    
+                </div>
+            </div>
+
+            <!--Comienzo-->
+            <div class="w3-container" id="aceite">
+                <!--  <div class="w3-container w3-text-grey" id="jeans">
+                    <p> </p>
+                </div> -->
+
+                <button type="button" onclick="javascript:imprim2();">Imprimir</button>
+   
+   
+   
+                <div id="buzon">
+                    <table id="buzon" class="tg" style="undefined;table-layout: fixed; width: 1062px"> 
+                            <colgroup>
+                                    <col style="width: 25px">
+                                    <col style="width: 100px">
+                                    <col style="width: 120px">
+                                    <col style="width: 80px">
+                                    <col style="width: 80px">
+                            </colgroup>
+
+                            <tr>
+                                    <th class="tg-lboi"># Factura</th>
+                                    <th class="tg-lboi">Cliente</th>
+                                    <th class="tg-lboi">Direccion</th>
+                                    <th class="tg-lboi">Fecha</th>
+                                    <th class="tg-lboi">Total</th>    
+                            </tr>
+
+                        <?php
+                    /* SELECT fc_vent_id, fc_fecha_vent,fc_vent_total, per_nombre, prod_nombre FROM `mh_fact_cabec_vent`, `mh_detal_vent`, 
+                        `mh_persons`,`mh_products`
+                        WHERE fc_vent_id=mh_fact_cabec_vent_fc_vent_id and per_id=mh_persons_per_id and prod_id=mh_products_prod_id*/
+
+                        $sql = "SELECT fc_vent_id,per_nombre,per_direccion, fc_fecha_vent,fc_vent_total
+                        FROM mh_fact_cabec_vent, mh_persons,mh_products,mh_detal_vent 
+                        WHERE fc_vent_id=mh_fact_cabec_vent_fc_vent_id 
+                        and $idFactura=fc_vent_id
+                        and mh_persons_per_id=$codigo and per_id=$codigo 
+                        and prod_id=mh_products_prod_id 
+                        GROUP BY mh_fact_cabec_vent_fc_vent_id";
+                        $result = $conn->query($sql);
+                        if ($result->num_rows > 0) {
+                            while ($row = $result->fetch_assoc()) {
+                            
+                                echo "<tr>";
+                                echo "<td>" .$row["fc_vent_id"] . "</td>";
+                                echo "<td>" .$row["per_nombre"] . "</td>";
+                                echo "<td>" .$row["per_direccion"] . "</td>";
+                                echo "<td>" .$row["fc_fecha_vent"] . "</td>";
+                                echo "<td>" .$row["fc_vent_total"] . "</td>";
+                            }
+                        } else {
+                            echo "<tr>";
+                            echo "<td colspan='5'>No existen productos</td>";
+                            echo "</tr>";
+                        }
+                        $conn->close();
+                        ?>
+                    </table>
+                <!-- </div> -->
+                <div id="buzon">
+                    <table id="buzon" class="tg" style="undefined;table-layout: fixed; width: 1062px"> 
+                        <colgroup>
+                                <col style="width: 120px">
+                                <col style="width: 100px">
+                                <col style="width: 75px">
+                                <col style="width: 75px">
+                        </colgroup>
+                        <tr>
+                                <th class="tg-lboi">Producto</th>
+                                <th class="tg-lboi">Precio</th>
+                                <th class="tg-lboi">Cantidad</th>
+                                <th class="tg-lboi">Subtotal</th>
+                        </tr>
+                        <?php
+                        /* SELECT fc_vent_id, fc_fecha_vent,fc_vent_total, per_nombre, prod_nombre FROM `mh_fact_cabec_vent`, `mh_detal_vent`, 
+                            `mh_persons`,`mh_products`
+                            WHERE fc_vent_id=mh_fact_cabec_vent_fc_vent_id and per_id=mh_persons_per_id and prod_id=mh_products_prod_id*/
+                            include '../../../config/conexionBD.php';
+                            $sql = "SELECT *
+                            FROM mh_fact_cabec_vent, mh_persons,mh_detal_vent, mh_products 
+                            WHERE fc_vent_id=mh_fact_cabec_vent_fc_vent_id 
+                            and $idFactura=fc_vent_id
+                            and mh_persons_per_id=$codigo 
+                            and per_id=$codigo 
+                            and prod_id=mh_products_prod_id 
+                            ";
+                            $result = $conn->query($sql);
+                            if ($result->num_rows > 0) {
+                                while ($row = $result->fetch_assoc()) {
+                                
+                                    echo "<tr>";
+                                    echo "<td>" .$row["prod_nombre"] . "</td>";
+                                    echo "<td>" .$row["fd_vent_precio"] . "</td>";
+                                    echo "<td>" .$row["fd_vent_cantidad"] . "</td>";
+                                    echo "<td>" .$row["fd_vent_total"] . "</td>";
+                                }
+                            } else {
+                                echo "<tr>";
+                                echo "<td colspan='4'>No existen productos</td>";
+                                echo "</tr>";
+                            }
+                            $conn->close();
+                        ?>
+                    </table>
+                <!--</div>-->
+
+                <div id="buzon">
+                    <table id="buzon" class="tg" style="undefined;table-layout: fixed; width: 1062px"> 
+                            <colgroup>
+                            <col style="width: 120px">
+                                <col style="width: 100px">
+                                <col style="width: 75px">
+                                <col style="width: 75px">
+                            </colgroup>
+
+                            <tr>
+                                    <th class="tg-lboi"></th>
+                                    <th class="tg-lboi"></th>
+                                    <th class="tg-lboi"></th>
+                                    <th class="tg-lboi">Total</th>    
+                            </tr>
+
+                        <?php
+                        /* SELECT fc_vent_id, fc_fecha_vent,fc_vent_total, per_nombre, prod_nombre FROM `mh_fact_cabec_vent`, `mh_detal_vent`, 
+                            `mh_persons`,`mh_products`
+                            WHERE fc_vent_id=mh_fact_cabec_vent_fc_vent_id and per_id=mh_persons_per_id and prod_id=mh_products_prod_id*/
+                            include '../../../config/conexionBD.php';
+                            $sql = "SELECT fc_vent_total
+                            FROM mh_fact_cabec_vent, mh_persons,mh_products,mh_detal_vent 
+                            WHERE fc_vent_id=mh_fact_cabec_vent_fc_vent_id
+                            and $idFactura=fc_vent_id
+                            and mh_persons_per_id=$codigo 
+                            and per_id=$codigo and prod_id=mh_products_prod_id 
+                            GROUP BY mh_fact_cabec_vent_fc_vent_id";
+                            $result = $conn->query($sql);
+                            if ($result->num_rows > 0) {
+                                while ($row = $result->fetch_assoc()) {
+                                
+                                    echo "<tr>";
+                                    echo "<td>" .$row["fc_vent_total"] . "</td>";
+                                }
+                            } else {
+                                echo "<tr>";
+                                echo "<td colspan='4'>No existen productos</td>";
+                                echo "</tr>";
+                            }
+                            $conn->close();
+                        ?>
+                    </table>
+                <!--</div>-->
+                   
+
+            </div>
+            <!----Final--->
+        </div>
+    </div>
+
     <main class="mainTabla">
-    <button type="button" onclick="javascript:imprim2();">Imprimir</button>
-   
-   
-   
-    <div id="buzon">
-        <table id="buzon" class="tg" style="undefined;table-layout: fixed; width: 1062px"> 
-                <colgroup>
-                        <col style="width: 25px">
-                        <col style="width: 100px">
-                        <col style="width: 120px">
-                        <col style="width: 80px">
-                        <col style="width: 80px">
-                </colgroup>
-
-                <tr>
-                        <th class="tg-lboi"># Factura</th>
-                        <th class="tg-lboi">Cliente</th>
-                        <th class="tg-lboi">Direccion</th>
-                        <th class="tg-lboi">Fecha</th>
-                        <th class="tg-lboi">Total</th>    
-                </tr>
-
-            <?php
-           /* SELECT fc_vent_id, fc_fecha_vent,fc_vent_total, per_nombre, prod_nombre FROM `mh_fact_cabec_vent`, `mh_detal_vent`, 
-            `mh_persons`,`mh_products`
-             WHERE fc_vent_id=mh_fact_cabec_vent_fc_vent_id and per_id=mh_persons_per_id and prod_id=mh_products_prod_id*/
-
-            $sql = "SELECT fc_vent_id,per_nombre,per_direccion, fc_fecha_vent,fc_vent_total
-            FROM mh_fact_cabec_vent, mh_persons,mh_products,mh_detal_vent 
-            WHERE fc_vent_id=mh_fact_cabec_vent_fc_vent_id 
-            and $idFactura=fc_vent_id
-             and mh_persons_per_id=$codigo and per_id=$codigo 
-             and prod_id=mh_products_prod_id 
-            GROUP BY mh_fact_cabec_vent_fc_vent_id";
-            $result = $conn->query($sql);
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                
-                      echo "<tr>";
-                      echo "<td>" .$row["fc_vent_id"] . "</td>";
-                      echo "<td>" .$row["per_nombre"] . "</td>";
-                      echo "<td>" .$row["per_direccion"] . "</td>";
-                      echo "<td>" .$row["fc_fecha_vent"] . "</td>";
-                      echo "<td>" .$row["fc_vent_total"] . "</td>";
-                }
-            } else {
-                echo "<tr>";
-                echo "<td colspan='5'>No existen productos</td>";
-                echo "</tr>";
-            }
-            $conn->close();
-            ?>
-        </table>
-   <!-- </div> -->
-    <div id="buzon">
-        <table id="buzon" class="tg" style="undefined;table-layout: fixed; width: 1062px"> 
-        <colgroup>
-                    <col style="width: 120px">
-                    <col style="width: 100px">
-                    <col style="width: 75px">
-                    <col style="width: 75px">
-        </colgroup>
-                    <tr>
-                        <th class="tg-lboi">Producto</th>
-                        <th class="tg-lboi">Precio</th>
-                        <th class="tg-lboi">Cantidad</th>
-                        <th class="tg-lboi">Subtotal</th>
-                    </tr>
-            <?php
-           /* SELECT fc_vent_id, fc_fecha_vent,fc_vent_total, per_nombre, prod_nombre FROM `mh_fact_cabec_vent`, `mh_detal_vent`, 
-            `mh_persons`,`mh_products`
-             WHERE fc_vent_id=mh_fact_cabec_vent_fc_vent_id and per_id=mh_persons_per_id and prod_id=mh_products_prod_id*/
-             include '../../../config/conexionBD.php';
-            $sql = "SELECT *
-            FROM mh_fact_cabec_vent, mh_persons,mh_detal_vent, mh_products 
-            WHERE fc_vent_id=mh_fact_cabec_vent_fc_vent_id 
-            and $idFactura=fc_vent_id
-            and mh_persons_per_id=$codigo 
-            and per_id=$codigo 
-            and prod_id=mh_products_prod_id 
-             ";
-            $result = $conn->query($sql);
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                
-                      echo "<tr>";
-                      echo "<td>" .$row["prod_nombre"] . "</td>";
-                      echo "<td>" .$row["fd_vent_precio"] . "</td>";
-                      echo "<td>" .$row["fd_vent_cantidad"] . "</td>";
-                      echo "<td>" .$row["fd_vent_total"] . "</td>";
-                }
-            } else {
-                echo "<tr>";
-                echo "<td colspan='4'>No existen productos</td>";
-                echo "</tr>";
-            }
-            $conn->close();
-            ?>
-        </table>
-    <!--</div>-->
-
-    <div id="buzon">
-        <table id="buzon" class="tg" style="undefined;table-layout: fixed; width: 1062px"> 
-                <colgroup>
-                <col style="width: 120px">
-                    <col style="width: 100px">
-                    <col style="width: 75px">
-                    <col style="width: 75px">
-                </colgroup>
-
-                <tr>
-                        <th class="tg-lboi"></th>
-                        <th class="tg-lboi"></th>
-                        <th class="tg-lboi"></th>
-                        <th class="tg-lboi">Total</th>    
-                </tr>
-
-            <?php
-           /* SELECT fc_vent_id, fc_fecha_vent,fc_vent_total, per_nombre, prod_nombre FROM `mh_fact_cabec_vent`, `mh_detal_vent`, 
-            `mh_persons`,`mh_products`
-             WHERE fc_vent_id=mh_fact_cabec_vent_fc_vent_id and per_id=mh_persons_per_id and prod_id=mh_products_prod_id*/
-            include '../../../config/conexionBD.php';
-            $sql = "SELECT fc_vent_total
-            FROM mh_fact_cabec_vent, mh_persons,mh_products,mh_detal_vent 
-            WHERE fc_vent_id=mh_fact_cabec_vent_fc_vent_id
-            and $idFactura=fc_vent_id
-             and mh_persons_per_id=$codigo 
-             and per_id=$codigo and prod_id=mh_products_prod_id 
-            GROUP BY mh_fact_cabec_vent_fc_vent_id";
-            $result = $conn->query($sql);
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                
-                      echo "<tr>";
-                      echo "<td>" .$row["fc_vent_total"] . "</td>";
-                }
-            } else {
-                echo "<tr>";
-                echo "<td colspan='4'>No existen productos</td>";
-                echo "</tr>";
-            }
-            $conn->close();
-            ?>
-        </table>
-    <!--</div>-->
+    
 <script>
 
     function imprim2(){
